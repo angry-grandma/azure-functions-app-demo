e7837e7ed47b7ce62fb44606be7077aa4d2b6d93
Stas Demchuk <standemchuk@gmail.com>
2018-06-05 21:24:01 +0300

    initial commit

---
+++ HttpTriggerJS/index.js
@@ -0,0 +1,39 @@
+const MongoClient = require('mongodb').MongoClient;
+const auth = require('../shared/index');
+
+module.exports = function (context, req) {
+    MongoClient.connect(
+        process.env.DBUrl,
+        { auth: auth },
+        (err, database) => {
+            if (err) {
+                context.log.error("Cannot connect to database");
+                context.done();
+            }
+            let idea = ({ id, name, description, color } = req.body);
+
+            var db = database.db(process.env.DBName);
+
+            db.collection('Ideas').insertOne(
+                {
+                    id: idea.id,
+                    name: idea.name,
+                    description: idea.description,
+                    color: idea.color
+                },
+                (err, result) => {
+                    if (err) {
+                        context.log.error("There was an issue with writing the new object to the database");
+                        context.done();
+                    }
+
+                    context.res = {
+                        body: idea
+                    }
+                    database.close();
+                    context.done();
+                }
+            )
+        }
+    );
+};
---
+++ shared/index.js
@@ -0,0 +1,6 @@
+module.exports = {
+    auth: {
+        user: process.env.DBUser,
+        password: process.env.DBPass
+    }
+}
